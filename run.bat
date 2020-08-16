c2168dc3b139aae31a16c3fb307f645c4ce8fb0e
Ankith Joseph <ankithjoseph@hotmail.com>
2020-08-16 09:53:20 +0530

    auto commit done 2020-08-16_09-53-12

--- run.bat
+++ run.bat
@@ -1,5 +1,11 @@
 @echo off
-REM SET /P _inputname= Please enter an identifier:
+REM SET /P _inputname= Please enter an identifier: 
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
 set /P loopcount=Please enter an identifier:
 :loop
@@ -26,6 +32,6 @@ goto loop
 :exitloop
 
 git add .
-git commit -am "."
+git commit -am "auto commit done %fullstamp%"
 git push
 pause
